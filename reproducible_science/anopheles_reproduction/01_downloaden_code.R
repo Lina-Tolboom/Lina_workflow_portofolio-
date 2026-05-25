202be10b614b905039f8895b6e79f1d81468bad0
Lina <tolboom.lina@gmail.com>
2026-05-25 17:00:12 +0200

    onderdeel 4 b gemaakt, was ik vergeten gelukkig nu achter gekomen en update van bookdown

---
+++ reproducible_science/anopheles_reproduction/01_downloaden_code.R
@@ -0,0 +1,13 @@
+# Download gedeelde code en data van het artikel
+
+download.file(
+  url = "https://raw.githubusercontent.com/SwissTPH/AnophelesModel/main/extdata/example_fig2.R",
+  destfile = "gedeelde_code/example_fig2.R",
+  mode = "wb"
+)
+
+download.file(
+  url = "https://raw.githubusercontent.com/SwissTPH/AnophelesModel/main/extdata/PNG_human_patterns.csv",
+  destfile = "gedeelde_code/PNG_human_patterns.csv",
+  mode = "wb"
+)
